5dc95e4cc68602e1f940c1e81d3eb43cca19d776
everardo sanchez <everardo.sanchez.adesis@gmail.com>
2015-02-19 17:05:11 -0600

    feat:(Intro): Agrega animaciones del Intro

--- develop/scripts/code.js
+++ develop/scripts/code.js
@@ -3,6 +3,9 @@ var THREE = require('threejs');
 var orbit = require('./orbitControls');
 
 $(function(){
+	
+
+	// **********************************************Código del Juego
 	orbit(THREE);
 	//variables para three
 	var renderer,
@@ -19,6 +22,21 @@ $(function(){
 		lose 	: false
 	};
 
+	var DomElements = {
+		arka : '',
+		barraDom :'',
+		circuloDom :'',
+		botonStart :''
+	};
+
+	var audios = {
+		inicio : new Audio('media/Loop1.mp3'),
+		adiosBarra : new Audio('media/Balloon Pop.mp3')
+	};
+
+	audios.inicio.loop=true;
+	audios.inicio.play();
+
 	var maxX = 150,
 			minY = -50,
 			maxY = 150,
@@ -26,6 +44,7 @@ $(function(){
 
 	var espaciado,
 		posBloque;
+
 
 	var setUp = function() {
 		renderer = new THREE.WebGLRenderer();
@@ -44,13 +63,16 @@ $(function(){
 		iniciaBarra();
 		iniciaBloques();
 		render();
-
-		console.log(scene.children);
 	};
 
 	var iniciaPelota = function() {
 		var geomPelota = new THREE.SphereGeometry(10,10,10);
-		var materialPelota = new THREE.MeshBasicMaterial({color:0xAF17E6,wireframe: true});
+		var materialTextura = new THREE.ImageUtils.loadTexture('../img/tri.jpg');
+		
+		materialTextura.minFilter = THREE.NearestFilter;
+		materialTextura.magFilter = THREE.NearestFilter;
+
+		var materialPelota = new THREE.MeshBasicMaterial({color:0xAF17E6,wireframe: false, map:materialTextura, side:THREE.DoubleSide, combine: true});
 		Pelota = new THREE.Mesh(geomPelota, materialPelota);
 		Pelota.position.y -=50;
 		Pelota.velocidad = 1;
@@ -66,7 +88,7 @@ $(function(){
 		for (i = 0;i< Game.bloques.length; i++) {
 			if( Game.bloques[i] === 1) {
 				var geomBloque = new THREE.BoxGeometry(espaciado, 10, 10);
-				var materialBloque = new THREE.MeshNormalMaterial({wireframe: true});
+				var materialBloque = new THREE.MeshNormalMaterial({wireframe: false});
 				Bloque = new THREE.Mesh(geomBloque, materialBloque);
 
 				posBloque = (i * espaciado) - 150;
@@ -78,7 +100,7 @@ $(function(){
 				scene.add(Bloque);
 			}
 		}
-		console.log(scene.children[9].position.x);
+		//console.log(scene.children[9].position.x);
 	};
 
 	var iniciaBarra = function () {
@@ -94,7 +116,7 @@ $(function(){
 
 	function mueveBarra() {
 		Barra.position.x = (event.pageX) - (window.innerWidth / 2);
-		//Barra.position.z = (event.pageY) - (window.innerHeight / 2);
+		// Barra.position.z = (event.pageY) - (window.innerWidth / 2);
 	}
 
 	var moverPelota = function () {
@@ -120,9 +142,9 @@ $(function(){
 				if ( (Pelota.position.x + 11) >= (scene.children[i-1].position.x - espaciado/2) && (Pelota.position.x + 11) <= (scene.children[i-1].position.x + espaciado/2) ) {
 					if( (Pelota.position.y + 11)  > (scene.children[i-1].position.y - 6) && (Pelota.position.y - 11)  < (scene.children[i-1].position.y + 6)  ) {
 						Pelota.masy = !Pelota.masy;	
-						Pelota.velocidad+=1;
+						Pelota.velocidad+=0.9;
 						scene.remove(scene.children[i-1]);	
-						console.log(Pelota.velocidad);
+						audios.adiosBarra.play();
 					}
 				}
 			}
@@ -144,7 +166,15 @@ $(function(){
 
 			if (Pelota.position.x <= (Barra.position.x + 35 ) && Pelota.position.x >= (Barra.position.x - 35)) 
 			{
-				Pelota.masy = true;
+				//if (Pelota.position.z <= (Barra.position.z + 10 ) && Pelota.position.z >= (Barra.position.z - 10)) 
+				//{
+					Pelota.masy = true;
+				/*	Pelota.masz = true;
+				}
+				else {
+					Game.lose = true;
+				}
+				*/
 			}
 			else {
 				Game.lose = true;
@@ -152,14 +182,14 @@ $(function(){
 			//else if (vidas !==0){
 				//vidas --;
 		}
-		/*		
-		if (Pelota.position.z == maxZ) {
+		
+		if (Pelota.position.z >= maxZ) {
 			Pelota.masz = false;
 		}
-		else if (Pelota.position.z == (maxZ*-1) ){
-			Pelota.masz = true;
+		else if (Pelota.position.z <= (maxZ*-1) ){
+			
 		}
-		*/
+		
 		if(Pelota.masx) {
 			Pelota.position.x += Pelota.velocidad;
 		}
@@ -191,5 +221,61 @@ $(function(){
 		requestAnimationFrame(render);
 	};
 
-	setUp();
+	function StartTheGame () {
+		setTimeout(function(){DomElements.circuloDom.top('-190%')}, 100);
+		setTimeout(function(){DomElements.arka.top('-170%')},  300);
+		setTimeout(function(){DomElements.barraDom.top('-150%')}, 500);
+		setTimeout(function(){DomElements.botonStart.top('-190%')}, 600);
+		
+		setTimeout(setUp, 3600);
+	};
+
+
+	var elementoDom = function(elem) {
+		this.elem = elem;
+	};
+
+	elementoDom.prototype.center = function (fromPositionW, fromPositionH) {
+		var mitadW = window.innerWidth/2;
+		var mitadH = window.innerHeight/2;
+
+		this.elem.css(fromPositionW, mitadW - parseInt( this.elem.css('width'))/2  );
+		this.elem.css(fromPositionH, mitadH - parseInt( this.elem.css('height'))/2  );
+	};
+
+	elementoDom.prototype.top = function (amount) {
+		this.elem.css('transition-duration','3s');
+		this.elem.css('transition-timing-function','ease-in');
+		this.elem.css('top', amount);
+	};
+
+
+	function animacionInicial() {
+		DomElements.arka = new elementoDom( $('#arkanoid') );
+		DomElements.barraDom = new elementoDom( $('#barra') );
+		DomElements.circuloDom = new elementoDom( $('#circulo') );
+		DomElements.botonStart = new elementoDom( $('#Start') );
+		
+
+		DomElements.arka.center('left', 'top');
+
+		setTimeout(function(){
+			DomElements.barraDom.center('left', 'top');
+		},600);
+
+		setTimeout(function(){
+			DomElements.circuloDom.center('right', 'top');
+		},400);
+
+		setTimeout(function(){
+			DomElements.botonStart.center('right', 'top');
+			DomElements.botonStart.elem.css('display','block');
+		},2500);
+
+		DomElements.botonStart.elem.on('click', StartTheGame);
+	}
+
+
+	animacionInicial();
+
 });
